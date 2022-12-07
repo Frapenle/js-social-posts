8806d162577c12d9d4b1fc9f08334d67a581d3bf
francescotrudu <francescotrudu84@gmail.com>
2022-12-07 16:11:31 +0100

    fix pic profile

--- script/main.js
+++ script/main.js
@@ -66,7 +66,7 @@ posts.forEach((element, index) => {
             <div class="post__header">
                 <div class="post-meta">                    
                     <div class="post-meta__icon">
-                        <img class="profile-pic" src="${element.media}" alt="${element.author.name} image">                    
+                        <img class="profile-pic" src="${element.author.image}" alt="${element.author.name} image">                    
                     </div>
                     <div class="post-meta__data">
                         <div class="post-meta__author">${element.author.name}</div>
@@ -76,7 +76,7 @@ posts.forEach((element, index) => {
             </div>
             <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
             <div class="post__image">
-                <img src="${element.author.image}" alt="">
+                <img src="${element.media}" alt="">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
